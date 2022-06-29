7b7f04fc16f7100ac7348e31f9ef3e99fe2a56b1
mykolassl <emykolas@gmail.com>
2022-06-29 19:39:25 +0300

    Finish html structure and styling

---
+++ main.js
@@ -0,0 +1,14 @@
+// const inputs = document.querySelectorAll('.input-container > input');
+// const inputBorderColor = inputs[0].style.border;
+// const submitBtn = document.querySelector('button[type="submit"]');
+
+// submitBtn.addEventListener('click', (e) => {
+//     inputs.forEach(input => {
+//         if (input.value === '') {
+//             input.style.border = '1px solid red';
+//         } else {
+//             input.style.border = inputBorderColor;
+
+//         }
+//     });
+// });
