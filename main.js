bd4888f1700a814e4c73a7909553153b92f0ba6f
mykolassl <emykolas@gmail.com>
2022-06-30 13:11:30 +0300

    Finish form validation

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-// const inputs = document.querySelectorAll('.input-container > input');
-// const inputBorderColor = inputs[0].style.border;
-// const submitBtn = document.querySelector('button[type="submit"]');
+const passwordField = document.querySelector('#password');
+const confirmPasswordField = document.querySelector('#confirm-password');
 
-// submitBtn.addEventListener('click', (e) => {
-//     inputs.forEach(input => {
-//         if (input.value === '') {
-//             input.style.border = '1px solid red';
-//         } else {
-//             input.style.border = inputBorderColor;
+function handlePasswordCheck(e) {
+    if(passwordField.value === confirmPasswordField.value) {
+        passwordField.style.borderColor = '#75ca2b';
+        confirmPasswordField.style.borderColor = '#75ca2b';
+        confirmPasswordField.nextSibling.remove();
+    } else {
+        passwordField.style.borderColor = 'red';
+        confirmPasswordField.style.borderColor = 'red';
 
-//         }
-//     });
-// });
+        // Check if error message already exists
+        if(!confirmPasswordField.nextElementSibling) {
+            let tempDiv = document.createElement('div');
+            tempDiv.classList.add('error');
+            tempDiv.innerText = 'Passwords do not match';
+            confirmPasswordField.insertAdjacentElement('afterend', tempDiv);
+        }
+    }
+}
+
+passwordField.addEventListener('keyup', handlePasswordCheck);
+confirmPasswordField.addEventListener('keyup', handlePasswordCheck);
